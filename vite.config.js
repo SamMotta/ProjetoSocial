81dba194ca6178d60823946d9266da70699d7803
SamMotta <samuelsamuca953@gmail.com>
2022-08-08 02:32:55 -0300

    Important infos at vite.config and .env.example

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => {
+    /** To test the production build on your machine, comment this if below, after that use yarn build 
+     * DON'T FORGET TO UNCOMMENT THE CODE, WHEN YOU BUILD TO GITHUB PAGES
+    */
     if (mode === "production") return { base: "https://sammotta.github.io/ProjetoSocial/" }
     return { base: "/" }
 })
